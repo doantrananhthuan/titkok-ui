612f09ccab45573a5c136ebdf818facdac132774
Thuan Doan <doantrananhthuan@gmail.com>
2023-06-19 20:40:35 +0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Button from './components/Button';
 
 function App() {
-  return (
-    <div>
-      <Button />
-      <h1>Hello Anh Em F8</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Button />
+            <h1>Hello Anh Em F8</h1>
+        </div>
+    );
 }
 
 export default App;
