af46e8c544721dcbe72a0b7c4fb0bf4b035dc950
Thuan Doan <doantrananhthuan@gmail.com>
2023-06-19 21:34:36 +0700

    cau hinh CSS/SASS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import Button from './components/Button';
-
 function App() {
-    return (
-        <div>
-            <Button />
-            <h1>Hello Anh Em F8</h1>
-        </div>
-    );
+   return (
+      <div className="App">
+         <h1>React App</h1>
+      </div>
+   );
 }
 
 export default App;
