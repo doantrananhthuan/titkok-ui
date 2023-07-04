af0da5cdd49f7bc92c3fd12c49d2cb02bba393e4
Thuan Doan <doantrananhthuan@gmail.com>
2023-07-04 21:21:05 +0700

    Tái cấu trúc và tối ưu code #1

---
+++ src/layouts/HeaderOnly/index.js
@@ -0,0 +1,14 @@
+import Header from '~/layouts/components/Header';
+
+function HearderOnly({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default HearderOnly;
